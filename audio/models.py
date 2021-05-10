629b352d1a1e8d4bb77eb312db5207796497d308
ajeshnishant <61043063+ajeshnishant@users.noreply.github.com>
2021-05-10 15:39:52 +0530

    Update models.py

--- audio/models.py
+++ audio/models.py
@@ -2,16 +2,19 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
-class Song(models.Model):
-    name = models.CharField(max_length=100)
+class Base(models.Model):
     duration_in_seconds = models.PositiveIntegerField()
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+class Song(Base):
+    name = models.CharField(max_length=100)
+
 
 
 class Podcast(models.Model):
-    name = models.CharField(max_length=100)
-    duration_in_seconds = models.PositiveIntegerField()
-    uploaded_at = models.DateTimeField(auto_now_add=True)
     host = models.CharField(max_length=100)
     participants = ArrayField(models.CharField(max_length=100), blank=True, size=10)
 
@@ -19,8 +22,7 @@ class Audiobook(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     narrator = models.CharField(max_length=100)
-    duration_in_seconds = models.PositiveIntegerField()
-    uploaded_at = models.DateTimeField(auto_now_add=True)
+
 
 
 
